850767a6005e201326d0a564deb3e4194827991f
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-03-10 20:32:24 +0800

    Add files via upload

--- worker_defects.py
+++ worker_defects.py
@@ -209,7 +209,7 @@ with col2:
     if selected_page != st.session_state.page:
         st.session_state.page = selected_page
         st.session_state.page_selector = selected_page
-        st.rerun()
+        # st.rerun()
 
 # Next button
 with col3:
